a768955ad78e286cbe3d9fb5a6cae0b5797f50d0
Karol Mirek <chemionpl@gmail.com>
2021-11-25 22:41:25 +0100

    secret is secret

--- default/settings.py
+++ default/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fzqs5yb$r(&-jhh5n3s*w%f*k5wxwi)#=$klgd61mp9-49mudg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
